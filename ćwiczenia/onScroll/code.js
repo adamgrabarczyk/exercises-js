28eed39c47e7ded87a225e29f759a8bf084b6941
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-04 16:56:09 +0200

    on scorll js done, display after srcoling 200px

--- ćwiczenia/onScroll/code.js
+++ ćwiczenia/onScroll/code.js
@@ -1,7 +1,37 @@
 
 
 
+window.onload = function () {
 
+    var topButton = document.getElementById('topButton');
+
+
+
+    window.onscroll = function () {
+        var scroll = document.getElementById('test');
+
+        var topButton = document.getElementById('topButton');
+
+        scroll.innerHTML = window.pageYOffset;
+
+
+        var yScroll = window.pageYOffset
+
+        if(yScroll > 200)
+topButton.style.display = "block";
+        else
+            topButton.style.display = "none";
+
+    };
+
+
+    topButton.onclick = function () {
+
+        window.scrollBy(0, -1 * window.pageYOffset);
+
+    }
+
+}
 
 
 
