098a4d37c1a47a63d347b28c9b44b92d16e05578
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-20 18:45:04 +0200

    find only one number

---
+++ ćwiczenia/WyrażeniaRegularne-RegExp/code.js
@@ -0,0 +1,30 @@
+/*
+ REGEXP
+ regular expression - regularne wyrażenia
+
+ stringDoPrzeszukania.search(wzór) - szuka i zwraca indeks
+ stringDoPrzeszukania.match(wzór) - szuka i zwraca w postaci tablicy
+ regExp.exec(stringDoPrzeszukania) - to samo co wyżej tyle, że nie działa flaga global i zwracana jest tylko jedna wartość
+ stringDoPrzeszukania.replace(wzór, "naCO"); - podmienia
+ wzór.test(stringDoPrzeszukania); - sprawdza, czy po prostu coś takiego się znajduje w stringu
+
+
+ g - global - po całym stringu
+ i - insensitive (nieczuły na wielkość znaków)
+
+ */
+
+
+
+window.onload = function () {
+
+
+    var info = document.getElementById('info');
+
+    var indeksy = "A-56 B-12  K-51 A-53";
+
+    var result = indeksy.match(/[0-9]/g);
+
+    info.innerHTML = result;
+
+}
