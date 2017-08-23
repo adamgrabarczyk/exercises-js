64182ad262b8af094146a3a35f8d12d29208f5cf
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-23 15:07:11 +0200

    interprate charter literally

--- ćwiczenia/WyrażeniaRegularne-RegExp/code.js
+++ ćwiczenia/WyrażeniaRegularne-RegExp/code.js
@@ -25,10 +25,11 @@ window.onload = function () {
 
     // var result = indeksy.match(/A\.[0-9]{1,}/g);
 
-    var imie = "Dsadhjasad";
+    var imie = "Dąręk";
 
-    var result = imie.match(/^[A-Z][a-zA-Z]+/g);
+    // var result = imie.match(/^[A-Z][a-zA-Z]+/g);
 
+    var  result =indeksy.match(/A\.?\d{1,}/g)
 
     info.innerHTML = result;
 
