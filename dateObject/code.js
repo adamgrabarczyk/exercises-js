b3fe53488fa202cb906af966682404698564788e
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-25 17:25:55 +0200

    display today date in good format

---
+++ dateObject/code.js
@@ -0,0 +1,25 @@
+/*
+
+ Manipulacja czasem z pomocą obiektu Date
+
+ new Date()
+ new Date(miliseconds) - ilość milisekund od 1 stycznia 1970 r
+ new Date(year, month, day, hours, minutes, seconds, milliseconds) - tylko 3 pierwsze argumenty są wymagane,
+ miesiące liczymy od 0
+ new Date(dateString) gdzie dateString to data podana w formacie stringu zgodnie z założonymi normami
+ np. YYYY-MM-DD, YYYY-MM, YYYY
+ YYYY/MM/DD, MM/DD/YYYY - UWAGA MIESIĄC PODAWAĆ PRZED DNIEM
+ */
+
+
+
+window.onload = function () {
+
+var info = document.getElementById("info");
+
+var today = new Date();
+
+info.innerHTML = today.getDate() + "/" + (today.getMonth()+1)+ "/" + today.getFullYear();
+
+
+}
