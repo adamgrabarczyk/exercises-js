83833ed9ec6dad043774465de28b29e84a862022
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-28 16:41:53 +0200

    math floor use to display number without '.'

---
+++ ćwiczenia/MathFunction/cytaty.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+window.onload = function () {
+
+    var info2 = document.getElementById("info2")
+
+    var cytaty = document.getElementById("cytaty")
+
+
+
+info2.innerHTML = MAth.floor(Math.random()*5);
+
+}
