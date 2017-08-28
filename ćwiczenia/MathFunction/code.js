0b7bf9b7d465b93ac78ced5e31ff30c20323513a
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-28 16:28:58 +0200

    introduction to math function

---
+++ ćwiczenia/MathFunction/code.js
@@ -0,0 +1,48 @@
+/*
+ Math.round() - zaokrąglenie do całości zgodnie z zasadami matematyki
+ Math.ceil() - sufit - zawsze do góry
+ Math.floor() - podłoga - zawsze w dół
+ toFixed(ileMiejscPoPrzecinku);
+
+ Math.abs() - wartość bezwględna z liczby
+ Math.pow(x,y) - x^y
+
+ parseInt
+ parseFloat("2.567hahaasfasf");
+ typeof
+
+ Math.random(); - zwraca losową liczbę od 0 do 1
+
+ */
+
+
+
+
+
+window.onload = function () {
+
+
+    var info = document.getElementById("info")
+
+    var x = 20.6653;
+
+    var y = -5;
+
+    // info.innerHTML = Math.round(x); zaokragla matematycznie do całości
+
+
+    // info.innerHTML = Math.floor(x); zaokrągla w dół
+
+
+    // info.innerHTML = x.toFixed(2); wskazuje ilosc miejsc po przecinku
+
+
+    // info.innerHTML = Math.abs(y); dziala jak wart.bezwzgledna i zmienia znak na dodatni
+
+    // info.innerHTML = Math.pow(y, 2); podnosi do potegi wskazanej w 2 argumencie
+
+
+    
+
+}
+
