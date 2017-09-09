4fbaa87d421d72441a807e967908ffa20a7ef4a9
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-09 13:50:45 +0200

    display 3 images useing table and for loop

---
+++ ćwiczenia/SlideShow/code.js
@@ -0,0 +1,26 @@
+
+
+window.onload = function () {
+
+
+    var slideShow = document.getElementById("images");
+
+
+    var imagesSrc = ["img1.png","img2.png","img3.png"];
+
+
+
+    for (var i = 0; i < imagesSrc.length; i++){
+
+
+        var image = new Image();
+
+        image.src = "images/"+imagesSrc[i];
+
+
+        slideShow.appendChild(image);
+
+    }
+
+
+}
