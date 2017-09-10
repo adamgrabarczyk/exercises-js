c497ba34f5a78f43707b37bd99deba468e9559b5
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-10 09:46:51 +0200

    make dynamic gallery with checked animation

--- ćwiczenia/JsImgGallery/code.js
+++ ćwiczenia/JsImgGallery/code.js
@@ -5,6 +5,38 @@
 window.onload = function () {
 
 
-    
+
+    var mainImage = document.getElementById("mainImage");
+
+    var image = new Image();
+
+    mainImage.appendChild(image);
+
+    var thumbnails = document.getElementsByClassName("thumbnail");
+
+    var currentThumbnail = thumbnails[0];
+
+image.src = currentThumbnail.getAttribute("src");
+
+currentThumbnail.className += " current"
+    // image.src = "img/img1.png";  poor solution
+
+
+
+    for (var i = 0; i < thumbnails.length; i++) {
+
+         thumbnails[i].onmouseover = function() {
+
+             currentThumbnail.className = currentThumbnail.className.replace("current", "");
+
+             currentThumbnail = this;
+
+             currentThumbnail.className += " current"; 
+
+image.src = this.getAttribute("src");
+
+         };
+    }
+
 
 }
