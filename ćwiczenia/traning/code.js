ebf9d698333e30a27de5d3f73b535a89b80eec2c
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-23 23:06:42 +0200

    build body function and add button name

--- ćwiczenia/traning/code.js
+++ ćwiczenia/traning/code.js
@@ -7,11 +7,15 @@ window.onload = function () {
 
     var result = document.getElementById("result");
 
+    var button = document.getElementById("button");
 
     var players = ["LeBron James", "Carmelo Anthony", "Dwane Wade", "Chirs Paul"]
 
 
+button.onclick = function () {
 
+        alert("siema");
+}
 
 
 
