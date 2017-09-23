07213d961ca5cc25c55b3ab5cbf7d36c4dc57385
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-23 23:13:05 +0200

    practise map function

--- ćwiczenia/traning/code.js
+++ ćwiczenia/traning/code.js
@@ -7,14 +7,32 @@ window.onload = function () {
 
     var result = document.getElementById("result");
 
+    var result1 = document.getElementById("result1");
+
+    var result2 = document.getElementById("result2");
+
+    var result3 = document.getElementById("result3");
+
     var button = document.getElementById("button");
+
+
+    var tables = [result, result1, result2, result3];
 
     var players = ["LeBron James", "Carmelo Anthony", "Dwane Wade", "Chirs Paul"]
 
 
 button.onclick = function () {
 
-        alert("siema");
+
+        tables.map(function () {
+
+            for(var i = 0; i < tables.length; i ++)
+            {
+                tables[i].innerHTML = players[i];
+
+            }
+        })
+
 }
 
 
