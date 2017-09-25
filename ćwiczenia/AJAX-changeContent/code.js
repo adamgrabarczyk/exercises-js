47446d3841ec839243f2e8e77b7b238beec0c861
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-25 22:25:24 +0200

    create varable which be a handler to href attribute from 'a' and use them when content should be change

--- ćwiczenia/AJAX-changeContent/code.js
+++ ćwiczenia/AJAX-changeContent/code.js
@@ -7,6 +7,15 @@ $(document).ready(function () {
 
     $('#content').load('content/index.html')
 
+$('ul#nav li a').click(function () {
+
+    var page = $(this).attr('href');
+
+    $('#content').load('content/' + page + '.html')
+
+    return false;
+
+});
 
 });
 
