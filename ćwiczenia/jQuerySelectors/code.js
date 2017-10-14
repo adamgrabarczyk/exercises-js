7a13d9cbbbfb86b8f5301e2fc87d4caa71a1dfb1
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-14 11:17:58 +0200

    take box tag + div

--- ćwiczenia/jQuerySelectors/code.js
+++ ćwiczenia/jQuerySelectors/code.js
@@ -10,7 +10,9 @@ function () {
 
     // $("div > b").css("color", "red"); // znak oznacza ze ma byc bezposrednie dziecko
 
-    $("div.a, li.b").css("color", "red");
+    // $("div.a, li.b").css("color", "red");
+
+    $("#box + div").css("color", "red");  // pobierz tagi div które są obok tagu z identfikatorem #box
 
 }
     
