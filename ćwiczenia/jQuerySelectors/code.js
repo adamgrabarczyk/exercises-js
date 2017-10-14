a4fefdcced79d05bf4564504dd69a84e6e52b3d7
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-14 11:13:17 +0200

    multiselector and multi class find

--- ćwiczenia/jQuerySelectors/code.js
+++ ćwiczenia/jQuerySelectors/code.js
@@ -8,7 +8,10 @@ $('document').ready(
 function () {
 
 
-    $("div > b").css("color", "red"); // znak oznacza ze ma byc bezposrednie dziecko
+    // $("div > b").css("color", "red"); // znak oznacza ze ma byc bezposrednie dziecko
+
+    $("div.a, li.b").css("color", "red");
+
 }
     
 );
