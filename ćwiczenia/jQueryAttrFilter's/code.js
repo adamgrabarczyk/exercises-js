8578f7b09f7fbc6b15421dfc8436b88c383e5512
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-16 17:06:18 +0200

    all images but git alt not

--- ćwiczenia/jQueryAttrFilter's/code.js
+++ ćwiczenia/jQueryAttrFilter's/code.js
@@ -7,10 +7,10 @@
 $('document').ready(
 function () {
 
+// $('img[alt]').css("border", "2px solid blue");
+// $('img[src="git.svg"]').css("border", "2px solid blue");
 
-$('img[src="git.svg"]').css("border", "2px solid blue");
-
-
+    $('img[alt != "git"]').css("border", "2px solid blue");
 
 }
     
