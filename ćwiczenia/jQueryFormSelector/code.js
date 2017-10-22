5a5748d2b2c5b006e8b495529911a1905e04db56
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-22 19:40:00 +0200

    first form selector

--- ćwiczenia/jQueryFormSelector/code.js
+++ ćwiczenia/jQueryFormSelector/code.js
@@ -1,4 +1,18 @@
-
+/*:button - wybiera wszystkie elementy których nazwa tagu to button lub typ to button.
+:checkbox - wybiera wszystkie elementy, których type to checkbox
+    :checked - dobiera elementy, które są zaznaczone.
+:disabled - wybiera elementy, które są disabled.
+:enabled - wybiera elementy, które są enabled.
+:file - wybiera elementy, których typ to file
+    :image - wybiera elementy, których typ to image
+    :input - wybiera elementy, w których można coś wpisywać (input, textarea, select, button)
+:password - wybiera elementy, których typ to hasło
+    :radio - wybiera elementy, których typ to radio
+    :reset - wybiera elementy, których typ to reset
+    :selected - wybiera elementy, które są zaznaczone
+    :submit - wybiera elementy, których typ to submit
+    :text - wybiera elementy, których typ to text
+*/
 
 
 
@@ -7,7 +21,7 @@
 $('document').ready(
 function () {
 
-
+$(":text").css("width", 500).css("position", "absolute").css("left", 100);
 
 
 }
