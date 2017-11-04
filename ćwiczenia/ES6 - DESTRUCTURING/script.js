192e29d37f5b3c98092a726444670e184f84baa4
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-04 17:28:19 +0100

    another way to destructure

--- ćwiczenia/ES6 - DESTRUCTURING/script.js
+++ ćwiczenia/ES6 - DESTRUCTURING/script.js
@@ -1,29 +1,38 @@
 
+//
+// var numbers = [1, 2, 3, 4, 5];
+//
+//
+//  var [a, b, c]  = numbers;
+//
+//  alert(a);
+// alert(b);
+// alert(c);
+//
+//
+// var x = 1;
+//
+// var y = 2;
+//
+// [x, y] = [y,x];
+//
+// alert(x);
+// alert(y);
+//
 
-var numbers = [1, 2, 3, 4, 5];
 
 
- var [a, b, c]  = numbers;
+var person = {
 
- alert(a);
-alert(b);
-alert(c);
+    name: 'tomek',
+    age: 30,
+    city: "Gdańsk"
 
-
-
-
+}
 
 
+var {name, age, city} = person;
 
-
-
-
-
-var x = 1;
-
-var y = 2;
-
-[x, y] = [y,x];
-
-alert(x);
-alert(y);
+alert(name);
+alert(age);
+alert(city);
