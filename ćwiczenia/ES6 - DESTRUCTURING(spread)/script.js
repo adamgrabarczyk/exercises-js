2019764b9e7a3b5de4167d7b9d683f9fd6f00b2c
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-04 18:25:21 +0100

    when param is array

--- ćwiczenia/ES6 - DESTRUCTURING(spread)/script.js
+++ ćwiczenia/ES6 - DESTRUCTURING(spread)/script.js
@@ -7,5 +7,16 @@ function sumValues(a, b, c) {
 
 var values = [3,2,7];
 
+// var sum = sumValues.apply(null, values)
 
-alert(sumValues(values));
+// alert(sumValues(...values));  // operator spread pozwala przekazac nam tablice jako parametr funkcji
+
+
+var array1 = [10,20,30];
+
+var array2 = [40,50,60];
+
+
+array1.push(...array2);
+
+alert(array1);
