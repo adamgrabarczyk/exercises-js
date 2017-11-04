9bbacf812fca2ca3c693932a5baebbfff337fe93
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-04 15:06:14 +0100

    let cant be double definied

--- ćwiczenia/ES6 - varables/script.js
+++ ćwiczenia/ES6 - varables/script.js
@@ -1,27 +1,27 @@
 
 
-
-function showOrderValue() {
-
-    var  orderValue = 320;
-
-    if ( orderValue >= 100) {
-
-        let newOrderValue = orderValue - orderValue *0.1 // zmienne definiowane za pomocą let maja swoj scope tylko w bloku w ktorym zostaly zdefioniowane
-     }
-
-    alert(newOrderValue);
-
-}
-
-
-showOrderValue();
-
-
-
-
-// let score = 10;
+//
+// function showOrderValue() {
+//
+//     var  orderValue = 320;
+//
+//     if ( orderValue >= 100) {
+//
+//         let newOrderValue = orderValue - orderValue *0.1 // zmienne definiowane za pomocą let maja swoj scope tylko w bloku w ktorym zostaly zdefioniowane
+//      }
+//
+//     alert(newOrderValue);
+//
+// }
 //
 //
-//
-// alert(score);
+// showOrderValue();
+
+
+
+
+let score = 10;
+
+let score = 20;
+
+console.log(score);
