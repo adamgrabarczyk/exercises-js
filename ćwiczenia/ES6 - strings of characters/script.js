fd74ea921958d5d53954655f5d6f5e7be7934e11
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-04 16:50:19 +0100

    new js methods

--- ćwiczenia/ES6 - strings of characters/script.js
+++ ćwiczenia/ES6 - strings of characters/script.js
@@ -11,3 +11,15 @@ let label = `Cena wynosi: ${price - price * discount} ${currency}`
 alert(label);
 
 
+
+
+
+const string = 'ES2015';
+
+alert(string.includes(2015));
+
+alert(string.startsWith('ES'), 0);
+
+alert(string.endsWith(2015));
+
+alert(string.repeat(4));
