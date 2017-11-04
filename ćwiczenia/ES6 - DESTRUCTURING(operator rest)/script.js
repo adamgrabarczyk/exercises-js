7265d1dc48ed309b0c5f400479bbbad11ce6c40e
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-04 18:01:29 +0100

    Rest operator in params

---
+++ ćwiczenia/ES6 - DESTRUCTURING(operator rest)/script.js
@@ -0,0 +1,11 @@
+
+
+//operator REST
+
+function getValue(...Args) {     // ... trzykropek mowi ze bierze pod uwage wszystkie argumenty ktore zostana przekazane do funkcji
+
+    alert(Args.length);
+}
+
+
+getValue(10,12,15);
