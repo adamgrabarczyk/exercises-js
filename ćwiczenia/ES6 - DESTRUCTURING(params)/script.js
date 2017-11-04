c014a9e8a4b9f75e67b42d5735380c56c93862ae
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-04 18:51:52 +0100

    use distructuring

--- ćwiczenia/ES6 - DESTRUCTURING(params)/script.js
+++ ćwiczenia/ES6 - DESTRUCTURING(params)/script.js
@@ -3,16 +3,22 @@
 
 
 
+//
+// function setOptions(options) {
+//
+//     const env = options.env
+//
+//     const db = options.db
+//
+// return [ env, db ];
+//
+// }
 
-function setOptions(options) {
-
-    const env = options.env
-
-    const db = options.db
-
-return [ env, db ];
+function setOptions({env, db}) {
+return [env, db];
 
 }
+
 
 let opt = setOptions({env: 'dev', db: 'SQL'})
 
