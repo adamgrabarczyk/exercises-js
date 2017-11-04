6b22fd5c7600c367f5dc53153a1abe31a0ceef78
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-04 21:14:07 +0100

    take out all chart

--- ćwiczenia/ES6 - Iterators & generators/script.js
+++ ćwiczenia/ES6 - Iterators & generators/script.js
@@ -6,7 +6,15 @@ var table = ['Adam', 'Tomek', 'Hubert', 'Maciej'];
 // for (let key in table) {
 //     console.log(key + " " + table[key]);
 // }
-
+                              // u gory operujemy na kluczach na dole na wartosciach
 for (let value of table){
-    console.log(value); 
+    console.log(value);
 }
+
+
+
+
+
+const name = "ksawery";
+
+for (const  char of name){console.log(char)};
