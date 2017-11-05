072c35abdf66a1d2a92536a11b686ef7e2fe9173
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-05 15:48:35 +0100

    somethig wrong with static method

--- ćwiczenia/ES6 - Class/script.js
+++ ćwiczenia/ES6 - Class/script.js
@@ -33,10 +33,31 @@ class Car {
         this.title = title;
     }
 
+    getId() {                       // moze byc wywolywane tylko na obiekcie danej klasy
+        return this.id;
+
+    }
+
+    static compareId (a, b){
+
+        const  id1 = a.id;
+        const  id2 = b.id;
+
+        if (id1 > id2) {
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
 }
 
 const car = new Car(897, 'siema');
 
+const car2 = new Car(567, 'papapa');
 
- console.log(car.id);
-console.log(car.title);
+ console.log(Article.compareId(car,car2));
+// console.log(car.getId());
+//  console.log(car.id);
+// console.log(car.title);
