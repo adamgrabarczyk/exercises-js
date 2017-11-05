74765734e0cebe229faf1bb3cfc62fb09c83b922
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-05 18:22:58 +0100

    simple promise

---
+++ ćwiczenia/ES6 - Promise/script.js
@@ -0,0 +1,20 @@
+
+// premise to obiekt/classa
+
+let x = 5;
+
+
+let p = new Promise(function (resolve, reject) {
+
+if (x = 1){
+    resolve('ok');
+}else {
+    reject(Error('błąd'));
+}
+
+});
+
+
+
+// console.log(p);
+
