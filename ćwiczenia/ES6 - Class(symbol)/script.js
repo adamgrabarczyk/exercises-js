72fc7e1613dad0c0a21cd1da03b23a23483631f4
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-05 16:22:21 +0100

    js symbol

---
+++ ćwiczenia/ES6 - Class(symbol)/script.js
@@ -0,0 +1,8 @@
+
+
+let s1 = Symbol('logowanie');   // symbole sa zawsze unikalne
+let s2 = Symbol('logowanie');
+
+console.log(s1 === s2);         // maja takie same wartosci ale nie sa identyczne
+
+
