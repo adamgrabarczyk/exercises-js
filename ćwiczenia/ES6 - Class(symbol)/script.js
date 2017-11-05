a74c75212173d993581bfb723b4b3044467bf1e5
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-05 16:25:41 +0100

    loop for in

--- ćwiczenia/ES6 - Class(symbol)/script.js
+++ ćwiczenia/ES6 - Class(symbol)/script.js
@@ -6,3 +6,19 @@ let s2 = Symbol('logowanie');
 console.log(s1 === s2);         // maja takie same wartosci ale nie sa identyczne
 
 
+
+
+
+
+let person = {
+    name: 'Adam',
+    age: 40,
+    city: "gdańsk"
+}
+
+
+// console.log(person);
+
+for (let i  in person) {
+    console.log(i);
+}
