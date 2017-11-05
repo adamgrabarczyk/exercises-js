514cda61d9dfa794f1ea6b64d8c84a118347460e
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-05 15:18:51 +0100

    second way to define class

--- ćwiczenia/ES6 - Class/script.js
+++ ćwiczenia/ES6 - Class/script.js
@@ -2,12 +2,21 @@
 
 
 
-class Article {
+// class Article {
+//
+// }
+//
+//
+//
+// const  art = new Article();
+//
+//
+//
 
-}
+
+const Article = class ArticleClass {}
 
 
+const art = new Article();
 
-const  art = new Article();
-
-
+console.log(art);
