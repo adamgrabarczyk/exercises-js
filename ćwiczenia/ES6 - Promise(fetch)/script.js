1847dbfe3670933d4d59be893af4800c03f47c63
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-05 18:54:20 +0100

    fetch img

---
+++ ćwiczenia/ES6 - Promise(fetch)/script.js
@@ -0,0 +1,21 @@
+
+
+// metoda fetch zwraca promise
+
+
+
+
+
+
+
+
+
+const image = document.querySelector('img');
+
+
+fetch('desktop.png').then(function (response) {
+
+    console.log(response);
+});
+
+
