7f4e378901d81e882a0a4a275bfc267cd0a3cce9
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-11-05 15:28:09 +0100

    constructor in class

--- ćwiczenia/ES6 - Class/script.js
+++ ćwiczenia/ES6 - Class/script.js
@@ -20,3 +20,23 @@ const Article = class ArticleClass {}
 const art = new Article();
 
 console.log(art);
+
+
+
+
+
+class Car {
+
+
+    constructor(id, title) {
+        this.id = id;
+        this.title = title;
+    }
+
+}
+
+const car = new Car(897, 'siema');
+
+
+ console.log(car.id);
+console.log(car.title);
